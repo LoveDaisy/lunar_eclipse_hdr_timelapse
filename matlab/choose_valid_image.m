c0744a5d6df2270a8a2c530098132bb6512dc3d3
Jiajie Zhang <zhangjiajie@kuaishou.com>
2018-02-05 15:17:33 +0800

    exposure compensation interpolate

--- matlab/choose_valid_image.m
+++ matlab/choose_valid_image.m
@@ -25,38 +25,54 @@ end
 files = files(idx);
 clear idx;
 
+expo_comp = [-0.67, 0, 0.67];
 image_store = struct('image', [], 'exposure', []);
-image_valid = false(total_images, 1);
+image_valid = false(total_images * 3, 1);
 for i = 1:total_images
     f_name = files(i).name;
     fprintf('Reading image %s...\n', f_name);
     
     img = imread(sprintf('%s/%s', image_path, f_name));
     max_value = intmax(class(img));
-    img_v = mean(img, 3);
-    p = prctile(img_v(:), [90, 95, 100]);
-    if p(3) < 0.6 * max_value
-        fprintf(' Intensity too low\n');
-        continue;
-    elseif p(1) > 0.22 * max_value
-        fprintf(' Intensity too high\n');
-        break;
+    img_v = mean(img, 3) / max_value;
+
+    valid_expo_comp = nan(3, 1);
+    for ei = 1:length(expo_comp)
+        img_v_ec = srgb_gamma(srgb_inverse_gamma(img_v) * 2^expo_comp(ei));
+
+        p = prctile(img_v_ec(:), [90, 95, 100]);
+        if p(3) < 0.6
+            fprintf(' Intensity too low\n');
+            continue;
+        elseif p(1) > 0.22
+            fprintf(' Intensity too high\n');
+            break;
+        end
+
+        valid_expo_comp(ei) = expo_comp(ei);
     end
 
-    [~, f_name, f_ext] = fileparts(f_name);
-    if ~strcmpi(f_ext, '.tif') && ~strcmpi(f_ext, '.tiff')
-        fprintf('No TIFF file, converting RAW to TIFF...\n');
-        system(sprintf('dcraw -v -r 1.95 1.0 1.63 1.0 -k 2047 -S 15490 -4 -T %s%s%s', image_path, f_name, f_ext));
-        fprintf('Reading TIFF file %s...\n', f_name);
-        img = imread(sprintf('%s%s%s', image_path, f_name, '.tiff'));
-%         fprintf('Removing temp TIFF file...\n');
-%         system(sprintf('rm %s%s%s', image_path, f_name, '.tiff'));
-    end
-        
+    if any(~isnan(valid_expo_comp))
+        [~, f_name, f_ext] = fileparts(f_name);
+        if ~strcmpi(f_ext, '.tif') && ~strcmpi(f_ext, '.tiff')
+            fprintf('No TIFF file, converting RAW to TIFF...\n');
+            system(sprintf('dcraw -v -r 1.95 1.0 1.63 1.0 -k 2047 -S 15490 -g 2.2 12.92 -4 -T %s%s%s', image_path, f_name, f_ext));
+            fprintf('Reading TIFF file %s...\n', f_name);
+            img = imread(sprintf('%s%s%s', image_path, f_name, '.tiff'));
+            fprintf('Removing temp TIFF file...\n');
+            system(sprintf('rm %s%s%s', image_path, f_name, '.tiff'));
+        end
+            
 
-    image_store(i, 1).image = img;
-    image_store(i, 1).exposure = exposure_store(i, :);
-    image_valid(i, 1) = true;
+        for ei = 1:length(expo_comp)
+            if isnan(expo_comp(ei))
+                continue;
+            end
+            image_store((i-1)*3+ei, 1).image = img;
+            image_store((i-1)*3+ei, 1).exposure = exposure_store(i, :) +[0, expo_comp(ei)];
+            image_valid((i-1)*3+ei, 1) = true;
+        end
+    end
 end
 image_store = image_store(image_valid);
 end
