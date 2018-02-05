f976401bc97b515f6ecf6516dc64184e6a60b867
Jiajie Zhang <zhangjiajie@kuaishou.com>
2018-02-05 10:14:08 +0800

    convert raw image

--- matlab/choose_valid_image.m
+++ matlab/choose_valid_image.m
@@ -25,8 +25,10 @@ clear idx;
 image_store = struct('image', [], 'exposure', []);
 image_valid = false(total_images, 1);
 for i = 1:total_images
-    fprintf('Reading image %s...\n', files(i).name);
-    img = imread(sprintf('%s/%s', image_path, files(i).name));
+    f_name = files(i).name;
+    fprintf('Reading image %s...\n', f_name);
+    
+    img = imread(sprintf('%s/%s', image_path, f_name));
     max_value = intmax(class(img));
     img_v = mean(img, 3);
     p = prctile(img_v(:), [90, 95, 100]);
@@ -37,6 +39,18 @@ for i = 1:total_images
         fprintf(' Intensity too high\n');
         break;
     end
+
+    [~, f_name, f_ext] = fileparts(f_name);
+    if strcmpi(f_ext, 'cr2')
+        fprintf('No TIFF file, converting RAW to TIFF...\n');
+        system(sprintf('dcraw -v -r 1.95 1.0 1.63 1.0 -k 2047 -S 15490 -g 2.4 12.92 -4 -T %s/%s.%s', image_path, f_name, f_ext));
+        fprintf('Reading TIFF file %s...\n', f_name);
+        img = imread(sprintf('%s/%s.%s', image_path, f_name, 'tiff'));
+        fprintf('Removing temp TIFF file...\n');
+        system(sprintf('rm %s/%s.%s', image_path, f_name, 'tiff'));
+    end
+        
+
     image_store(i, 1).image = img;
     image_store(i, 1).exposure = exposure_store(i, :);
     image_valid(i, 1) = true;
