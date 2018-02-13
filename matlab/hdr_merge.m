20fa853eee2e0124a0a5abddcfe779466b31e00e
Jiajie Zhang <zhangjiajie@kuaishou.com>
2018-02-14 00:27:51 +0800

    changes

--- matlab/hdr_merge.m
+++ matlab/hdr_merge.m
@@ -10,7 +10,7 @@ if total_images == 1
 end
 
 ref_ind = min(3, total_images);
-total_w = 0;
+total_w = 0; merge_img = 0;
 for i = 1:total_images
     fprintf('Merging image %d/%d...\n', i, total_images);
     img = im2double(imtranslate(image_store(i).image, -trans_mat(:, i, ref_ind)'));
@@ -22,19 +22,21 @@ for i = 1:total_images
     w = intensity_w;
     total_w = total_w + w;
     
-    img_pyr = generate_laplacian_pyramid(img, pyr_layers, pyr_sig);
-    w_pyr = generate_gaussian_pyramid(w, pyr_layers, pyr_sig);
-    for j = 1:length(img_pyr)
-        img_pyr{j} = bsxfun(@times, img_pyr{j}, w_pyr{j});
-    end
-    
-    if ~exist('merge_pyr', 'var')
-        merge_pyr = img_pyr;
-    else
-        for j = 1:length(merge_pyr)
-            merge_pyr{j} = merge_pyr{j} + img_pyr{j};
-        end
-    end
+%     img_pyr = generate_laplacian_pyramid(img, pyr_layers, pyr_sig);
+%     w_pyr = generate_gaussian_pyramid(w, pyr_layers, pyr_sig);
+%     for j = 1:length(img_pyr)
+%         img_pyr{j} = bsxfun(@times, img_pyr{j}, w_pyr{j});
+%     end
+%     
+%     if ~exist('merge_pyr', 'var')
+%         merge_pyr = img_pyr;
+%     else
+%         for j = 1:length(merge_pyr)
+%             merge_pyr{j} = merge_pyr{j} + img_pyr{j};
+%         end
+%     end
+
+    merge_img = merge_img + bsxfun(@times, img, intensity_w);
     
 %     total_w_pyr = generate_gaussian_pyramid(total_w, pyr_layers, pyr_sig);
 %     tmp_pyr = merge_pyr;
@@ -55,9 +57,10 @@ for i = 1:total_images
 %     drawnow;
 end
 
-total_w_pyr = generate_gaussian_pyramid(total_w, pyr_layers, pyr_sig);
-for j = 1:length(merge_pyr)
-    merge_pyr{j} = bsxfun(@times, merge_pyr{j}, 1./total_w_pyr{j});
-end
-merge_result = collapse_laplacian_pyramid(merge_pyr);
+% total_w_pyr = generate_gaussian_pyramid(total_w, pyr_layers, pyr_sig);
+% for j = 1:length(merge_pyr)
+%     merge_pyr{j} = bsxfun(@times, merge_pyr{j}, 1./total_w_pyr{j});
+% end
+% merge_result = collapse_laplacian_pyramid(merge_pyr);
+merge_result = bsxfun(@times, merge_img, 1./total_w);
 end
